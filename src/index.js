23b75458a45037eeb6b187a640e9a2b63d5afdfc
magicdawn <magicdawn@qq.com>
2019-12-17 22:06:41 +0800

    chore: fix typo

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ globalify({ MglMarker, MglPopup })
 
 // source
 import MglSource from './component/MglSource.js'
-import MglLayer from './component//MglLayer.js'
+import MglLayer from './component/MglLayer.js'
 export { MglSource, MglLayer }
 globalify({ MglSource, MglLayer })
 
